02863854b7f1fbb88e923e01a656bc3505b2963b
Dennis <dennisli1197@gmail.com>
2018-09-23 16:02:24 -0400

    change to Hi Ek'

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 public class Test {
   public static main void(String[] args) {
-    System.out.println("hi dennis");
+    System.out.println("hi ek");
     System.out.println("difference");
   }
 }
