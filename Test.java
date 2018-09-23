45284a4e6f3c27aacec809a7e2710a6ff4c5ef9f
Dennis <dennisli1197@gmail.com>
2018-09-23 16:18:37 -0400

    changed to ekchung

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 public class Test {
   public static main void(String[] args) {
-    System.out.println("hi ek");
+    System.out.println("hi ekchung");
     System.out.println("difference");
   }
 }
