47cc399624014dac8eb2aa9df6079c61f6e70e48
ek <chungek@brandeis.edu>
2018-09-28 14:49:59 -0400

    cmdBuilder updated with more checks

--- src/cs131/pa1/filter/sequential/SequentialREPL.java
+++ src/cs131/pa1/filter/sequential/SequentialREPL.java
@@ -1,6 +1,7 @@
 package cs131.pa1.filter.sequential;
 
 
+import java.util.List;
 import java.util.Scanner;
 
 import cs131.pa1.filter.Message;
@@ -17,10 +18,12 @@ public class SequentialREPL {
 		System.out.print(Message.WELCOME);
 		System.out.print(Message.NEWCOMMAND);
 		String userCommands = console.nextLine();
-		currentWorkingDirectory = "";
+		currentWorkingDirectory = System.getProperty("user.dir");
+		System.out.println(currentWorkingDirectory);
 		while(!userCommands.equals("exit")) {
-			SequentialCommandBuilder current = new SequentialCommandBuilder(userCommands, currentWorkingDirectory);
+			SequentialCommandBuilder currBuild = new SequentialCommandBuilder(userCommands, currentWorkingDirectory);
 			// procvess and print out output  from command
+			List<SequentialFilter> currCmds = currBuild.createFiltersFromCommand();
 			
 			
 			System.out.print(Message.NEWCOMMAND);
