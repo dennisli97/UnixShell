b2b77cc258dd4db765f9cb8e96f23e017e8575b0
ek <chungek@brandeis.edu>
2018-09-30 18:09:34 -0400

    bc dennis messed up

--- cs131a_PA1_Part1_StarterKit/cs131a_PA1_Part1_StarterKit/src/cs131/pa1/filter/sequential/CAT.java
+++ cs131a_PA1_Part1_StarterKit/cs131a_PA1_Part1_StarterKit/src/cs131/pa1/filter/sequential/CAT.java
@@ -3,48 +3,62 @@ package cs131.pa1.filter.sequential;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 import cs131.pa1.filter.Message;
 
 public class CAT extends SequentialFilter{
 	private String filesNames;
+	private Queue<String> filesQ;
 	
 	// constructor to initialize the filter
 	public CAT(String filesNames) {
 		// if there is multiple files to cat, add them separately in the input queue
 		this.filesNames = filesNames;
 		String[] files = filesNames.split(" ");
-		input = new LinkedList<String>();
+		filesQ = new LinkedList<String>();
 		output = new LinkedList<String>();
 		for(int i=0; i<files.length;i++) {
-			input.add(files[i]);
+			filesQ.add(files[i]);
 		}
 	}
 	
+	
+	
 	public void process() {
-		String currentWorkingDirectory = SequentialREPL.currentWorkingDirectory;
-		File[] files = new File(currentWorkingDirectory).listFiles();// list all file in the directory
-		boolean foundFile; // to check if file is found in the directory
-		while (!input.isEmpty()){
-			foundFile = false;// reassaign every time there is a new file to be find
-			String fileName = input.poll();
-			for (File file : files) {// go through file to find the desire file 
-			    if (file.getName().equals(fileName)) {
-			        File toCat = new File(currentWorkingDirectory+"/"+fileName);
-			        try {
-						readFile(toCat);
-						foundFile = true;
-					} catch (FileNotFoundException e) {
-						// exception has been handled
-					}
-			    }
+
+//		if(input != null) {// if 
+//			System.out.print(Message.CANNOT_HAVE_INPUT.with_parameter("cat "+filesNames));
+//			return;
+//		}
+		if(input == null) {
+			String currentWorkingDirectory = SequentialREPL.currentWorkingDirectory;
+			File[] files = new File(currentWorkingDirectory).listFiles();// list all file in the directory
+			boolean foundFile; // to check if file is found in the directory
+			while (!filesQ.isEmpty()){
+				foundFile = false;// reassaign every time there is a new file to be find
+				String fileName = filesQ.poll();
+				for (File file : files) {// go through file to find the desire file 
+				    if (file.getName().equals(fileName)) {
+				        File toCat = new File(currentWorkingDirectory+"/"+fileName);
+				        try {
+							readFile(toCat);
+							foundFile = true;
+						} catch (FileNotFoundException e) {
+							// exception has been handled
+						}
+				    }
+				}
+				if(foundFile == false) {// if the file is not found in the directory, error message
+					System.out.print(Message.FILE_NOT_FOUND.with_parameter("cat " + filesNames));
+					return;
+				}
 			}
-			if(foundFile == false) {// if the file is not found in the directory, error message
-				System.out.print(Message.FILE_NOT_FOUND.with_parameter("CAT " + filesNames));
-				return;
-			}
+		} else {
+			System.out.print(Message.CANNOT_HAVE_INPUT.with_parameter("cat "+filesNames));
 		}
+		
 		
 	}
 	
@@ -65,3 +79,4 @@ public class CAT extends SequentialFilter{
 		return null;
 	}
 }
+
--- cs131a_PA1_Part1_StarterKit/cs131a_PA1_Part1_StarterKit/src/cs131/pa1/filter/sequential/PWD.java
+++ cs131a_PA1_Part1_StarterKit/cs131a_PA1_Part1_StarterKit/src/cs131/pa1/filter/sequential/PWD.java
@@ -2,12 +2,19 @@ package cs131.pa1.filter.sequential;
 
 import java.util.LinkedList;
 
+import cs131.pa1.filter.Message;
+
 public class PWD extends SequentialFilter{
 	
 	public void process() {
 		// add the current working directory to the output queue
-		output = new LinkedList<String>();
-		output.add(SequentialREPL.currentWorkingDirectory);
+		
+		if(input == null) {
+			output = new LinkedList<String>();
+			output.add(SequentialREPL.currentWorkingDirectory);
+		} else {
+			System.out.print(Message.CANNOT_HAVE_INPUT.with_parameter("pwd"));
+		}
 	}
 
 	@Override
