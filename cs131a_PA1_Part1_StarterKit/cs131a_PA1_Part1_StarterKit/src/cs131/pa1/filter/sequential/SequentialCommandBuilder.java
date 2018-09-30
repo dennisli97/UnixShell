6ac6e6e95ced136264ed6936bd8df3ef1104b4c9
ek <chungek@brandeis.edu>
2018-09-30 17:25:50 -0400

    build

--- cs131a_PA1_Part1_StarterKit/cs131a_PA1_Part1_StarterKit/src/cs131/pa1/filter/sequential/SequentialCommandBuilder.java
+++ cs131a_PA1_Part1_StarterKit/cs131a_PA1_Part1_StarterKit/src/cs131/pa1/filter/sequential/SequentialCommandBuilder.java
@@ -80,6 +80,9 @@ public class SequentialCommandBuilder {
 					if (multSubs) {
 						containsBrokenCmds = true;
 						System.out.print(Message.INVALID_PARAMETER.with_parameter(cmd + " " + subCmd));
+					} else if (subCmd.equals("")) {
+						containsBrokenCmds = true;
+						System.out.print(Message.REQUIRES_PARAMETER.with_parameter(cmd));
 					}
 				//4: check cmds that require param
 				} else if (cmd.equals("cat") || cmd.equals("grep") || cmd.equals(">")) {
