334cb74078c6e6479fc083758916ed5bf53cf4e4
ek <chungek@brandeis.edu>
2018-09-30 18:20:51 -0400

    build

--- cs131a_PA1_Part1_StarterKit/cs131a_PA1_Part1_StarterKit/src/cs131/pa1/filter/sequential/LS.java
+++ cs131a_PA1_Part1_StarterKit/cs131a_PA1_Part1_StarterKit/src/cs131/pa1/filter/sequential/LS.java
@@ -3,10 +3,18 @@ package cs131.pa1.filter.sequential;
 import java.io.File;
 import java.util.LinkedList;
 
-public class LS extends SequentialFilter{
+import cs131.pa1.filter.Message;
 
-	public void process() {
+public class LS extends SequentialFilter{
+	public LS() {
 		output = new LinkedList<String>();
+	}
+	
+	public void process() {
+		if(input != null) {
+			System.out.print(Message.CANNOT_HAVE_INPUT.with_parameter("ls"));
+			return;
+		}
 		String currentWorkingDirectory = SequentialREPL.currentWorkingDirectory;
 		// array to hold all files and add each file anme to the output queue
 		File curr = new File(currentWorkingDirectory);
